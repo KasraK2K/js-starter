93b2d28b7401e5b27fe1ff8af782c4ba342396cf
Kasra Karami <Kasra.Karami.Work@gmail.com>
2022-07-07 21:12:16 +0430

    change comment file in RabbitMQ

--- src/common/integrations/rabbitmq/index.js
+++ src/common/integrations/rabbitmq/index.js
@@ -1,3 +1,18 @@
+// ──────────────────────────────────────────────────────────────────────
+//   :::::: H O W   T O   U S E : :  :   :    :     :        :          :
+/* ──────────────────────────────────────────────────────────────────────
+import RabbitMQ from 'src/common/integrations/rabbitmq'
+const rabbitmq = new RabbitMQ('amqp://username:password@localhost:5672')
+rabbitmq.producer({ name: 'Kasra' })
+rabbitmq.consumer(
+	(data) =>
+		new Promise((resolve, reject) => {
+			console.log(`Consume:\n${JSON.stringify(data, null, 2)}\n`)
+			return data ? resolve('this is true') : reject('this is false')
+		})
+)
+────────────────────────────────────────────────────────────────────── */
+
 import RabbitMQ from './RabbitMQ'
 
 export default RabbitMQ
